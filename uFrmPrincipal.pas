5d2e1f788819c31475bc298473f38745490a66fa
delmar.apui@gmail.com <delmar.apui@gmail.com>
2022-04-18 23:01:12 -0400

    Melhoria do projeto / esta em desenvolvimento. aguarde os proximos commits

--- uFrmPrincipal.pas
+++ uFrmPrincipal.pas
@@ -21,16 +21,40 @@ uses
   System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, IdBaseComponent,
   IdComponent, IdTCPConnection, IdTCPClient, IdHTTP, IdIOHandler,
-  IdIOHandlerSocket, IdIOHandlerStack, IdSSL, IdSSLOpenSSL, System.JSON;
+  IdIOHandlerSocket, IdIOHandlerStack, IdSSL, IdSSLOpenSSL, System.JSON,
+  Vcl.ExtCtrls, Vcl.ComCtrls, Vcl.Buttons;
 
 type
   TFrmPrincipal = class(TForm)
-    btnEnviarBoleto: TButton;
+    pnPrincipal: TPanel;
+    pnBotao: TPanel;
+    sbSalvar: TSpeedButton;
+    spLineBtn: TShape;
+    Panel1: TPanel;
+    PageControl1: TPageControl;
+    TabSheet1: TTabSheet;
+    TabSheet2: TTabSheet;
+    TabSheet3: TTabSheet;
+    pnlToolbar: TPanel;
+    lblTitle: TLabel;
+    Panel4: TPanel;
+    Shape3: TShape;
+    Label9: TLabel;
     mReturn: TMemo;
-    btnGetToken: TButton;
-    edtToken: TEdit;
+    Panel2: TPanel;
+    edtKeyFile: TEdit;
+    Label2: TLabel;
+    Label1: TLabel;
+    edtCertFile: TEdit;
+    Panel3: TPanel;
+    Shape1: TShape;
+    Label3: TLabel;
+    sbCertFile: TSpeedButton;
+    sbKeyFile: TSpeedButton;
     procedure btnEnviarBoletoClick(Sender: TObject);
     procedure btnGetTokenClick(Sender: TObject);
+    procedure sbCertFileClick(Sender: TObject);
+    procedure sbKeyFileClick(Sender: TObject);
   private
     function GetToken: String;
     function PostBoleto(aToken: String): Boolean;
@@ -41,7 +65,21 @@ type
 
 var
   FrmPrincipal: TFrmPrincipal;
-  aToken: String;
+
+Const
+  // Local do certificado digital, Obtido na tela de aplicações no IB
+  CERT_FILE = 'C:\Inter API_Certificado.crt';
+  // Local da chave key, Obtido na tela de aplicações no IB
+  KEY_FILE = 'C:\Inter API_Chave.key';
+  // Client Id obtido no detalhe da tela de aplicações no IB
+  CLIENT_ID = '';
+  // Client Secret obtido no detalhe da tela de aplicações no IB
+  CLIENT_SECRET = '';
+  // Escopos cadastrados na tela de aplicações.
+  SCOPE = 'extrato.read boleto-cobranca.read boleto-cobranca.write';
+  // Link conforme documentação
+  URI_TOKEN = 'https://cdpj.partners.bancointer.com.br/oauth/v2/token';
+  URI_BOLETOS = 'https://cdpj.partners.bancointer.com.br/cobranca/v2/boletos';
 
 implementation
 
@@ -49,27 +87,23 @@ implementation
 
 procedure TFrmPrincipal.btnEnviarBoletoClick(Sender: TObject);
 begin
-  if PostBoleto(edtToken.Text) then
+  {if PostBoleto(edtToken.Text) then
   begin
     mReturn.Lines.Add('-----------------------');
     mReturn.Lines.Add('Success');
-  end;
+  end;}
 end;
 
 function TFrmPrincipal.GetToken: String;
 var
   Params: TStringList;
   Resp: TStringStream;
-  URI, CLIENT_ID, CLIENT_SECRET, SCOPE: String;
   Jso: TJSONObject;
   JsoPair: TJSONPair;
   HTTP: TIdHTTP;
   IOHandle: TIdSSLIOHandlerSocketOpenSSL;
 begin
   try
-    CLIENT_ID := ''; //Client Id obtido no detalhe da tela de aplicações no IB
-    CLIENT_SECRET := ''; //Client Secret obtido no detalhe da tela de aplicações no IB
-    SCOPE := 'extrato.read boleto-cobranca.read boleto-cobranca.write'; //Escopos cadastrados na tela de aplicações.
     Params := TStringList.Create;
     Resp := TStringStream.Create;
 
@@ -80,58 +114,47 @@ begin
     IOHandle := TIdSSLIOHandlerSocketOpenSSL.Create(HTTP);
     IOHandle.SSLOptions.Method := sslvTLSv1_2;
     IOHandle.SSLOptions.Mode := sslmClient;
-    IOHandle.SSLOptions.CertFile :=
-      'C:\Inter API_Certificado.crt'; //Local do certificado digital, Obtido na tela de aplicações no IB
-    IOHandle.SSLOptions.KeyFile :=
-      'C:\Inter API_Chave.key'; //Local do Key, Obtido na tela de aplicações no IB
+    IOHandle.SSLOptions.CertFile := CERT_FILE;
+    IOHandle.SSLOptions.KeyFile := KEY_FILE;
     HTTP.IOHandler := IOHandle;
     HTTP.Request.ContentType := 'application/x-www-form-urlencoded';
     HTTP.Request.CharSet := 'UTF-8';
 
     try
-      URI := 'https://cdpj.partners.bancointer.com.br/oauth/v2/token';
-
       Params.Add('client_id=' + CLIENT_ID);
       Params.Add('client_secret=' + CLIENT_SECRET);
       Params.Add('scope=' + SCOPE);
       Params.Add('grant_type=client_credentials');
 
-      try
-        HTTP.Post(URI, Params, Resp);
-        mReturn.Lines.Add(Resp.DataString);
-        if HTTP.ResponseCode = 200 then
+      HTTP.Post(URI_TOKEN, Params, Resp);
+      if HTTP.ResponseCode = 200 then
+      begin
+        if Resp.DataString <> '' then
         begin
-          if Resp.DataString <> '' then
+          Jso := TJSONObject.Create;
+          Jso.Parse(Resp.Bytes, 0);
+
+          for JsoPair in Jso do
           begin
-            Jso := TJSONObject.Create;
-            Jso.Parse(Resp.Bytes, 0);
-
-            for JsoPair in Jso do
-            begin
-              if JsoPair.JsonString.Value = 'access_token' then
-                Result := JsoPair.JsonValue.Value;
-            end;
-
-            Jso.Free;
+            if JsoPair.JsonString.Value = 'access_token' then
+              Result := JsoPair.JsonValue.Value;
           end;
-        end
-        else
-        begin
-          Result := 'Error: ' + IntToStr(HTTP.ResponseCode) + ' - ' +
-            Resp.DataString;
-        end;
-      except
-        on E: EIdHTTPProtocolException do
-          Result := 'Error: ' + E.ErrorMessage;
-      end;
 
+          Jso.Free;
+        end;
+      end
+      else
+      begin
+        mReturn.Lines.Add('Error: ' + IntToStr(HTTP.ResponseCode) + ' - ' +
+          Resp.DataString);
+      end;
     finally
       FreeAndNil(Params);
       FreeAndNil(Resp);
     end;
   except
-    on ex: exception do
-      Result := 'Erro ao gerar token: ' + ex.Message;
+    on e: exception do
+      mReturn.Lines.Add('Erro ao gerar token: ' + e.Message);
   end;
 end;
 
@@ -139,7 +162,7 @@ function TFrmPrincipal.PostBoleto(aToken: String): Boolean;
 var
   Params: TStringList;
   JsonStreamEnvio, Resp: TStringStream;
-  URI, CLIENT_ID, CLIENT_SECRET, SCOPE, Parametros: String;
+  Parametros: String;
   Jso: TJSONObject;
   JsoPair: TJSONPair;
   HTTP: TIdHTTP;
@@ -157,10 +180,8 @@ begin
     IOHandle := TIdSSLIOHandlerSocketOpenSSL.Create(HTTP);
     IOHandle.SSLOptions.Method := sslvTLSv1_2;
     IOHandle.SSLOptions.Mode := sslmClient;
-    IOHandle.SSLOptions.CertFile :=
-      'C:\Inter API_Certificado.crt'; //Local do certificado digital, Obtido na tela de aplicações no IB
-    IOHandle.SSLOptions.KeyFile :=
-      'C:\Inter API_Chave.key'; //Local do Key, Obtido na tela de aplicações no IB
+    IOHandle.SSLOptions.CertFile := CERT_FILE;
+    IOHandle.SSLOptions.KeyFile := KEY_FILE;
     HTTP.IOHandler := IOHandle;
     HTTP.Request.ContentType := 'application/json';
     HTTP.Request.Accept := 'application/json';
@@ -169,17 +190,16 @@ begin
     HTTP.Request.CustomHeaders.Add('Authorization: Bearer ' + aToken);
 
     try
-      URI := 'https://cdpj.partners.bancointer.com.br/cobranca/v2/boletos';
       Parametros := '{' + '"seuNumero": "' + '32' + '",' + '"valorNominal": ' +
         '25' + ', "valorAbatimento": 0,' + '"dataVencimento": "' + '2022-04-15'
         + '",' + '"numDiasAgenda": 60,' + '"pagador": {' + '"cpfCnpj": "' +
         '33958520000160' + '",' + '"tipoPessoa": "' + 'JURIDICA' + '",' +
-        '"nome": "' + 'AMIL CONTABILIDADE LTDA' + '",' +
-        '"endereco": "' + 'RUA BAHIA' + '",' + '"numero": "' + '1006' + '",' +
-        '"complemento": "' + '' + '",' + '"bairro": "' + 'CENTRO' + '",' +
-        '"cidade": "' + 'APUI' + '",' + '"uf": "' + 'AM' + '",' + '"cep": "' +
-        '69265000' + '",' + '"email": "' + 'atendimento@amil.cnt.br' + '",' + '"ddd": "' + '' + '",' +
-        '"telefone": "' + '' + '"},' + '"mensagem": {' +
+        '"nome": "' + 'AMIL CONTABILIDADE LTDA' + '",' + '"endereco": "' +
+        'RUA BAHIA' + '",' + '"numero": "' + '1006' + '",' + '"complemento": "'
+        + '' + '",' + '"bairro": "' + 'CENTRO' + '",' + '"cidade": "' + 'APUI' +
+        '",' + '"uf": "' + 'AM' + '",' + '"cep": "' + '69265000' + '",' +
+        '"email": "' + 'atendimento@amil.cnt.br' + '",' + '"ddd": "' + '' + '",'
+        + '"telefone": "' + '' + '"},' + '"mensagem": {' +
         '"linha1": "Honorario Contabil Ref.: ' + '69265000' + '"},' +
         '"desconto1": {' + '"codigoDesconto": "PERCENTUALDATAINFORMADA",' +
         '"data": "' + '2022-04-10' + '",' + '"taxa": ' + '5' + ',' +
@@ -187,10 +207,10 @@ begin
         '"data": "' + '2022-04-16' + '",' + '"valor": 0,' + '"taxa": 2},' +
         '"mora": {' + '"codigoMora": "TAXAMENSAL",' + '"data": "' + '2022-04-16'
         + '",' + '"valor": 0,' + '"taxa": 1' + '}}';
-      Params.text := Parametros;
-      JsonStreamEnvio := TStringStream.Create(Params.text);
+      Params.Text := Parametros;
+      JsonStreamEnvio := TStringStream.Create(Params.Text);
       try
-        HTTP.Post(URI, JsonStreamEnvio, Resp);
+        HTTP.Post(URI_BOLETOS, JsonStreamEnvio, Resp);
         mReturn.Lines.Add(Resp.DataString);
         if HTTP.ResponseCode = 200 then
         begin
@@ -213,12 +233,12 @@ begin
         end
         else
         begin
-         mReturn.Lines.Add('Error: ' + IntToStr(HTTP.ResponseCode) + ' - ' +
+          mReturn.Lines.Add('Error: ' + IntToStr(HTTP.ResponseCode) + ' - ' +
             Resp.DataString);
         end;
       except
-        on E: EIdHTTPProtocolException do
-         mReturn.Lines.Add('Error: ' + E.ErrorMessage);
+        on e: EIdHTTPProtocolException do
+          mReturn.Lines.Add('Error: ' + e.ErrorMessage);
       end;
 
     finally
@@ -231,9 +251,47 @@ begin
   end;
 end;
 
+procedure TFrmPrincipal.sbCertFileClick(Sender: TObject);
+var
+  openDialog: topendialog;
+begin
+  openDialog := topendialog.Create(self);
+  try
+    openDialog.InitialDir := GetCurrentDir;
+    openDialog.Options := [ofFileMustExist];
+    openDialog.Filter := 'Certificado|*.crt';
+    openDialog.FilterIndex := 1;
+    if openDialog.Execute then
+    begin
+      edtCertFile.Text := openDialog.FileName;
+    end;
+  finally
+    openDialog.Free;
+  end;
+end;
+
+procedure TFrmPrincipal.sbKeyFileClick(Sender: TObject);
+var
+  openDialog: topendialog;
+begin
+  openDialog := topendialog.Create(self);
+  try
+    openDialog.InitialDir := GetCurrentDir;
+    openDialog.Options := [ofFileMustExist];
+    openDialog.Filter := 'Chave Key|*.key';
+    openDialog.FilterIndex := 1;
+    if openDialog.Execute then
+    begin
+      edtKeyFile.Text := openDialog.FileName;
+    end;
+  finally
+    openDialog.Free;
+  end;
+end;
+
 procedure TFrmPrincipal.btnGetTokenClick(Sender: TObject);
 begin
-  edtToken.Text := GetToken;
+  //edtToken.Text := GetToken;
 end;
 
 end.
